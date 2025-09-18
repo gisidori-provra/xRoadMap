a9b392768f97a4d60f26f084ff4ed66d36dc4a5f
GIsidori <45291585+GIsidori@users.noreply.github.com>
2025-09-18 12:57:21 +0200

    Aggiunta di classi e proprietà per ciclabili
    Sono state aggiunte nuove classi e proprietà nel file `Ciclabile.cs`, che rappresentano vari attributi e dettagli relativi a ciclabili, come `NoteShp`, `Immagini`, `Dettaglio`, `FonteCompilazione`, e altri. Queste modifiche includono anche annotazioni per la documentazione e la persistenza dei dati.
    
    È stata modificata la struttura del file `Model.DesignedDiffs.xafml`, con l'aggiunta di un nuovo nodo per le ciclabili e la riorganizzazione dei gruppi di layout per riflettere le nuove proprietà e la loro disposizione nell'interfaccia utente.
    
    Nel file `xRoadMap23.Module.csproj`, è stata inclusa una nuova classe `Comune.cs`, che definisce le entità `Comune`, `Provincia` e `Regione`, con le relative proprietà e annotazioni per la persistenza.

---
+++ xRoadMap23.Module/BusinessObjects/Comune.cs
@@ -0,0 +1,108 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xRoadMap.Module.BusinessObjects
+{
+    [DeferredDeletion(false)]
+    [NavigationItem("Impostazioni")]
+    public class Comune : XPCustomObject
+    {
+        public Comune(Session session) : base(session)
+        { }
+
+        [Key]
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string CodiceIstat
+        {
+            get => codiceIstat;
+            set => SetPropertyValue(nameof(CodiceIstat), ref codiceIstat, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Nome
+        {
+            get => nome;
+            set => SetPropertyValue(nameof(Nome), ref nome, value);
+        }
+
+        
+        public Provincia Provincia
+        {
+            get => provincia;
+            set => SetPropertyValue(nameof(Provincia), ref provincia, value);
+        }
+
+        Provincia provincia;
+        string codiceIstat;
+        string nome;
+
+    }
+
+    [DeferredDeletion(false)]
+    [NavigationItem("Impostazioni")]
+    public class Provincia : XPCustomObject
+    {
+        public Provincia(Session session) : base(session)
+        { }
+
+        [Key]
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string CodiceIstat
+        {
+            get => codiceIstat;
+            set => SetPropertyValue(nameof(CodiceIstat), ref codiceIstat, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Nome
+        {
+            get => nome;
+            set => SetPropertyValue(nameof(Nome), ref nome, value);
+        }
+
+        
+        public Regione Regione
+        {
+            get => regione;
+            set => SetPropertyValue(nameof(Regione), ref regione, value);
+        }
+        Regione regione;
+        string codiceIstat;
+        string nome;
+    }
+
+    [DeferredDeletion(false)]
+    [NavigationItem("Impostazioni")]
+    public class Regione : XPCustomObject
+    {
+        public Regione(Session session) : base(session)
+        { }
+
+
+        string nome;
+        string codiceIstat;
+
+        [Key]
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string CodiceIstat
+        {
+            get => codiceIstat;
+            set => SetPropertyValue(nameof(CodiceIstat), ref codiceIstat, value);
+        }
+
+        
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Nome
+        {
+            get => nome;
+            set => SetPropertyValue(nameof(Nome), ref nome, value);
+        }
+
+    }
+}
